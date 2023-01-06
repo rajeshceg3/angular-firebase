84a9536fc949bfa98cf719f9b4b1aa48e30a4ec0
rajeshceg3 <rajeshceg3@gmail.com>
2023-01-06 21:59:15 +0530

    2 files modified, 1 added

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "your-api-key",
+    authDomain: "firestarter-c60bf.firebaseapp.com",
+    projectId: "firestarter-c60bf",
+    storageBucket: "firestarter-c60bf.appspot.com",
+    messagingSenderId: "130016012775",
+    appId: "1:130016012775:web:d5132425274b6695cfb542",
+    measurementId: "G-4JDRQF2XJF"
+  }
+
+}
